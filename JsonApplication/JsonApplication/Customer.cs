8de85100c6918f23ca4521c209ab657a0fea5d1d
marciomonzon <marcio.pcmonzon@gmail.com>
2022-10-22 14:24:05 -0300

    serialization and deserialization json objects

---
+++ JsonApplication/JsonApplication/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace JsonApplication
+{
+    [Serializable]
+    public class Customer
+    {
+        public string CustomerName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public decimal TotalSales { get; set; }
+        public DateTime FinalPurchaseDate { get; set; }
+    }
+}
---
+++ JsonApplication/JsonApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JsonApplication;
+using Newtonsoft.Json;
+
+var customers = new List<Customer>
+{
+    new Customer
+    {
+        CustomerName = "John",
+        Age = 33,
+        Email = "john@gmail.com",
+        TotalSales = 4000,
+        FinalPurchaseDate = new DateTime(2021, 3, 29)
+    },
+    new Customer
+    {
+        CustomerName = "George",
+        Age = 32,
+        Email = "george@gmail.com",
+        TotalSales = 6000,
+        FinalPurchaseDate = new DateTime(2021, 2, 12)
+    },
+    new Customer
+    {
+        CustomerName = "Peter",
+        Age = 28,
+        Email = "peter@gmail.com",
+        TotalSales = 2000,
+        FinalPurchaseDate = new DateTime(2021, 1, 10)
+    }
+};
+
+var customer = new Customer
+{
+    CustomerName = "James",
+    Age = 20,
+    Email = "james@gmail.com",
+    TotalSales = 5000,
+    FinalPurchaseDate = new DateTime(2021, 1, 5)
+};
+
+var customerJson = JsonConvert.SerializeObject(customer);
+Console.WriteLine(customerJson);
+
+var json = JsonConvert.DeserializeObject<Customer>(customerJson);
+
+Console.ReadKey();
